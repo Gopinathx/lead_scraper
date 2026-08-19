f0279fffc80240cec22e61a00b6f54af092d0801
Gopinath A <0206gopinath@gmail.com>
2026-08-20 00:43:09 +0530

    Fix playwright install command by removing --with-deps

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-# Install Playwright browser binary & dependencies
-playwright install chromium --with-deps
+# Install ONLY the chromium browser binary without requiring root privileges
+playwright install chromium
